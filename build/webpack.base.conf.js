61bce56c9888f9524c1fc0a27e814e07bce802f4
heweijie <babaa1f4@163.com>
2021-02-01 17:58:59 +0800

    modify webpack

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -66,7 +66,7 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        include: [resolve('src'), resolve('test')]
+        include: [resolve('src'), resolve('test')]//,resolve('node_modules/vue-contextmenujs')
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
